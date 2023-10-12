c757912b17842e306fb5556ec0d6d5aba55a2c13
Douglas Machado <douglas.machado@luizalabs.com>
2023-10-12 17:01:14 -0300

    feat: criado documentação do código

--- README.md
+++ README.md
@@ -1,23 +1,12 @@
-# Teste de caixa branca
+# Código de um provavel sistema de autenticação em Java
 
-## Observe o código no slide 28 da aula com conteúdo de teste de caixa branca;
-[![N|Code](https://github.com/douglads/teste-caixa-branca/blob/main/code.JPG?raw=true)](https://github.com/douglads/teste-caixa-branca)
-## Aponte os erros que podem conter no código;
-As variaveis nome e result não fazem sentido ficarem fora dos escopos das funções
-A instrução SQL não precisa necessariamente ser quebrada varias vezes e alterado seu valor.
-Nos catchs os erros não são tratados
+Este é um repositório que contém um código Java para autenticação de usuários em um banco de dados MySQL. O código foi documentado para facilitar o entendimento e manutenção.
 
-## Considere as recomendações dos slides posterior ao enunciado do exercício (slide 29);
+## Estrutura do Repositório
 
-- ### A DOCUMENTAÇÃO FOI DESCRITA NO CÓDIGO?
-    Não, ele não descreve o que ocorre no código quase não há comentários
-- ### AS VARIÁVEIS E CONSTANTES POSSUEM BOA NOMENCLATURA?
-    As variáveis e constantes não possuem uma boa nomenclatura. Por exemplo, a variável "conn" poderia ser renomeada para "connection" para tornar seu propósito mais claro.
-- ### EXISTEM LEGIBILIDADE E ORGANIZAÇÃO NO CÓDIGO?
-    Sim, o código esta bem descrito e legivel, porém, a ressalvas é recomendado utilizar indentação adequada, separar as instruções SQL em linhas diferentes e adicionar espaços entre operadores e palavras-chave.
-- ### TODOS OS NULLPOINTERS FORAM TRATADOS?
-    Não há tratamento de null pointers no código fornecido. Seria necessário adicionar verificações para evitar possíveis exceções.
-- ### A ARQUITETURA UTILIZADA FOI DEVIDAMENTE RESPEITADA?
-    Não, pois, aqui só analisamos uma class e não o projeto como um todo.
-- ### AS CONEXÕES UTILIZADAS FORAM FECHADAS?
-    As conexões utilizadas não estão sendo fechadas corretamente. É recomendado utilizar blocos try-finally para garantir o fechamento adequado das conexões.
+- `User.java`: Classe Java que lida com a autenticação de usuários no banco de dados MySQL.
+- `README.md`: Este arquivo explicando o propósito do repositório e fornecendo instruções.
+
+## Documentação
+
+A documentação completa do código está disponível na classe `User` e pode ser acessada no arquivo [User.java](User.java).
--- User.java
+++ User.java
@@ -1,43 +1,71 @@
-package Login;
+/**
+ * A classe User representa um usuário e fornece métodos para autenticação
+ * e interação com um banco de dados MySQL.
+ */
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+package Login; // Define o pacote da classe
 
-public class User {
-    
-    public Connection conectarBD(){
+import java.sql.Connection; // Importa a classe Connection
+import java.sql.DriverManager; // Importa a classe DriverManager
+import java.sql.ResultSet; // Importa a classe ResultSet
+import java.sql.Statement; // Importa a classe Statement
+
+public class User { // Declara a classe User
+
+    /**
+     * Conecta ao banco de dados MySQL.
+     *
+     * @return A conexão com o banco de dados ou null em caso de erro.
+     */
+    public Connection conectarBD() {
         Connection conn = null;
         try {
+            // Carrega o driver JDBC do MySQL
             Class.forName("com.mysql.Driver.Manager").newInstance();
+            
+            // URL de conexão com o banco de dados
             String url = "jdbc.mysql://127.0.0.1/test?user=lopes&password=123";
+            
+            // Estabelece a conexão com o banco de dados
             conn = DriverManager.getConnection(url);
-        } 
-        catch (Exception e) {
+        } catch (Exception e) {
+            // Lida com exceções, como ClassNotFoundException, SQLException, etc.
         }
-        return conn;
+        return conn; // Retorna a conexão
     }
 
+    // Variáveis de instância
     public String nome = "";
     public boolean result = false;
 
-    public boolean verificarUsuario(String login, String senha){
-        String sql = "";
-        Connection conn = conectarBD();
-        //INSTRUÇÃO SQL
+    /**
+     * Verifica se um usuário com o login e a senha fornecidos existe no banco de dados.
+     *
+     * @param login O nome de usuário.
+     * @param senha A senha do usuário.
+     * @return true se o usuário existe, false caso contrário.
+     */
+    public boolean verificarUsuario(String login, String senha) {
+        String sql = ""; // Instrução SQL
+        Connection conn = conectarBD(); // Conecta ao banco de dados
+        
+        // INSTRUÇÃO SQL para verificar a existência do usuário
         sql += "select nome from usuarios ";
         sql += "where login ='" + login + "' ";
         sql += "and senha ='" + senha + "';";
+        
         try {
-            Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery(sql);
+            Statement st = conn.createStatement(); // Cria um objeto Statement para a consulta
+            ResultSet rs = st.executeQuery(sql); // Executa a consulta
+            
+            // Se um resultado for encontrado, o usuário existe
             if (rs.next()) {
-                result = true;
-                nome = rs.getString(nome);
+                result = true; // Define o resultado como true
+                nome = rs.getString(nome); // Obtém o nome do usuário
             }
         } catch (Exception e) {
+            // Lida com exceções, como SQLException, SQLException, etc.
         }
-        return result;
+        return result; // Retorna o se o usuário existe ou não
     }
-}//fim da class
+}
